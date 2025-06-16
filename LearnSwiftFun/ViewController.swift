0b07cbb33cf410c4fe219f7ba75af1a9918deb3c
Piyush Rathi <piyush.rathi@3disystems.com>
2025-06-16 14:22:22 +0530

    Initial Commit

---
+++ LearnSwiftFun/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LearnSwiftFun
+//
+//  Created by Piyush Rathi on 16/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .cyan
+    }
+}
+
